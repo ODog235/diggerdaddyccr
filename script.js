a35ebf722cc6d79a9a4383d688cdbe57095ba455
ODog235 <110572426+ODog235@users.noreply.github.com>
2023-04-27 11:33:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var userinput
+var order_num = 0
+function order() {
+    var userinput = confirm("Would you like to buy it?");
+    if(userinput == true){
+        alert("Your order has been placed!");
+    }
+}
